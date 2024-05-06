e9ab1b4a2e56883cc00608efaab9593540f23931
Ominousss <ahmedabdalla7192@gmail.com>
2024-05-06 22:31:09 +0100

    Got search to show up usernames

--- app/src/main/java/vinigarstudios/fitfinder/search/Search.java
+++ app/src/main/java/vinigarstudios/fitfinder/search/Search.java
@@ -1,15 +1,29 @@
 package vinigarstudios.fitfinder.search;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import vinigarstudios.fitfinder.R;
+import vinigarstudios.utility.FirebaseHelper;
 import vinigarstudios.utility.VinigarCompatActivity;
 
 public class Search extends VinigarCompatActivity
@@ -18,7 +32,7 @@ public class Search extends VinigarCompatActivity
     //region Private Fields
 
     private ListView listView;
-    private String[] placeHolderItems;
+    private List<String> itemsList;
     private ArrayAdapter<String> arrayAdapter;
 
     //endregion
@@ -32,9 +46,10 @@ public class Search extends VinigarCompatActivity
         this.setContentView(R.layout.activity_search);
 
         this.listView = findViewById(R.id.searchUserListView);
-        this.placeHolderItems = new String[]{"Test", "Test2"};
+        this.itemsList = FirebaseHelper.GetListFromDatabase("profiles", "profileName");
 
-        this.arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, placeHolderItems);
+
+        this.arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemsList);
         this.listView.setAdapter(arrayAdapter);
     }
 
