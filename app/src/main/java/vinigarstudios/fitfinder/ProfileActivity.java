169751c68fb9194ab39879e82b2b227921e8dbec
Cathal <cathalcrowepatrick@gmail.com>
2024-05-06 23:24:00 +0100

    Navigation Bar
    navigation bar that allows you to navigate between all pages

--- app/src/main/java/vinigarstudios/fitfinder/ProfileActivity.java
+++ app/src/main/java/vinigarstudios/fitfinder/ProfileActivity.java
@@ -2,7 +2,10 @@ package vinigarstudios.fitfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_profile);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.bottom_home) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.bottom_friends) {
+                startActivity(new Intent(getApplicationContext(), FriendsActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.bottom_upload) {
+                startActivity(new Intent(getApplicationContext(), UploadActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else return item.getItemId() == R.id.bottom_profile;
+        });
+
     }
 }
