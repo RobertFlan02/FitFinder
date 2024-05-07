a240785a7241c3b651c12aae71ecfa6e74a4e80e
Ominousss <ahmedabdalla7192@gmail.com>
2024-05-08 00:55:25 +0100

    Upgraded gradle packages that caused dependency bugs (Duplicate Classes)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,19 +35,19 @@ android {
         jvmTarget = "1.8"
     }
 }
-
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:22.3.1")
-    implementation("com.google.firebase:firebase-firestore:24.11.1")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
+
     implementation("com.firebaseui:firebase-ui-firestore:8.0.2")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
-    implementation ("com.google.firebase:firebase-storage:20.3.0")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("com.google.firebase:firebase-storage:21.0.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.google.firebase:firebase-messaging:24.0.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     testImplementation("junit:junit:4.13.2")
