a26947d7fd030dc9170c959c3fac15c27ef7bc73
Ominousss <ahmedabdalla7192@gmail.com>
2024-05-10 01:02:47 +0100

    Filtering by friends now works. Filter toggle switch added.

--- app/src/main/java/vinigarstudios/fitfinder/FriendsActivity.java
+++ app/src/main/java/vinigarstudios/fitfinder/FriendsActivity.java
@@ -1,27 +1,31 @@
 package vinigarstudios.fitfinder;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
 import vinigarstudios.fitfinder.adapter.SearchUserRecyclerAdapter;
 import vinigarstudios.fitfinder.models.UserModel;
-import vinigarstudios.fitfinder.search.SearchUserActivity;
 import vinigarstudios.utility.FirebaseHelper;
 import vinigarstudios.utility.VinigarCompatActivity;
 
@@ -31,17 +35,25 @@ public class FriendsActivity extends VinigarCompatActivity
     private SearchUserRecyclerAdapter recyclerAdapter;
     private TextView friendsText;
     private TextView textViewProfileTitle;
+    private SwitchCompat friendsFilterSwitch;
     private LinearLayout topBar;
     private BottomNavigationView bottomNavigationView;
+    private String getLastText;
+    private boolean isFriendsFilterOn;
+
+    private UserModel currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_friends);
         this.recyclerView = findViewById(R.id.searchUserRecyclerList);
         this.friendsText = findViewById(R.id.friendsActivityText);
+        this.friendsFilterSwitch = findViewById(R.id.friendsSearchFilterButton);
         this.bottomNavigationView = findViewById(R.id.bottomNavigation);
         this.textViewProfileTitle = findViewById(R.id.textViewProfileTitle);
         this.topBar = findViewById(R.id.profile_title_layout);
+
+        this.friendsFilterSwitch.setVisibility(View.INVISIBLE);
 
         bottomNavigationView.setSelectedItemId(R.id.bottom_friends);
         bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -81,6 +93,7 @@ public class FriendsActivity extends VinigarCompatActivity
             public void onViewAttachedToWindow(@NonNull View v) {
                 //Search was opened.
                 recyclerView.setVisibility(View.VISIBLE);
+                friendsFilterSwitch.setVisibility(View.VISIBLE);
                 SetFriendPageVisibility(View.INVISIBLE);
             }
 
@@ -88,7 +101,30 @@ public class FriendsActivity extends VinigarCompatActivity
             public void onViewDetachedFromWindow(@NonNull View v) {
                 //Search was closed.
                 recyclerView.setVisibility(View.INVISIBLE);
+                friendsFilterSwitch.setVisibility(View.INVISIBLE);
                 SetFriendPageVisibility(View.VISIBLE);
+            }
+        });
+
+        friendsFilterSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                FirebaseHelper.GetCurrentUserDetails().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        currentUser = task.getResult().toObject(UserModel.class);
+                        if (isChecked)
+                        {
+                            isFriendsFilterOn = true;
+                            SetupFriendSearchRecyclerView(getLastText);
+                        }
+                        else
+                        {
+                            isFriendsFilterOn = false;
+                            SetupSearchRecyclerView(getLastText);
+                        }
+                    }
+                });
             }
         });
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -101,7 +137,21 @@ public class FriendsActivity extends VinigarCompatActivity
             @Override
             public boolean onQueryTextChange(String newText)
             {
-                SetupSearchRecyclerView(newText);
+                getLastText = newText;
+                if (!isFriendsFilterOn)
+                {
+                    SetupSearchRecyclerView(newText);
+                }
+                else
+                {
+                    FirebaseHelper.GetCurrentUserDetails().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            currentUser = task.getResult().toObject(UserModel.class);
+                            SetupFriendSearchRecyclerView(newText);
+                        }
+                    });
+                }
                 return false;
             }
         });
@@ -135,6 +185,34 @@ public class FriendsActivity extends VinigarCompatActivity
         Query query = FirebaseHelper.GetAllProfilesCollectionReference()
                 .whereGreaterThanOrEqualTo("username",searchTerm)
                 .whereLessThanOrEqualTo("username",searchTerm+'\uf8ff');
+
+        FirestoreRecyclerOptions<UserModel> options = new FirestoreRecyclerOptions.Builder<UserModel>()
+                .setQuery(query, UserModel.class).build();
+
+        recyclerAdapter = new SearchUserRecyclerAdapter(options, getApplicationContext());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerAdapter.startListening();
+    }
+
+    private void SetupFriendSearchRecyclerView(String searchTerm)
+    {
+        Query query;
+
+        this.recyclerView = findViewById(R.id.searchUserRecyclerList);
+        if (currentUser.getFriendsId().isEmpty())
+        {
+            //This query will return empty. There is an error when getFriendsId is empty so we have to handle it in this if statement.
+            query = FirebaseHelper.GetAllProfilesCollectionReference()
+                    .whereEqualTo("userId", "");
+        }
+        else
+        {
+            query = FirebaseHelper.GetAllProfilesCollectionReference()
+                    .whereGreaterThanOrEqualTo("username",searchTerm)
+                    .whereLessThanOrEqualTo("username",searchTerm+'\uf8ff')
+                    .whereIn("userId", currentUser.getFriendsId());
+        }
 
         FirestoreRecyclerOptions<UserModel> options = new FirestoreRecyclerOptions.Builder<UserModel>()
                 .setQuery(query, UserModel.class).build();
